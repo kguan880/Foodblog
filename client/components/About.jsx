c43f731778d856aa20667932b0b641adb0d6a93c
Kevin Guan <kevinguan97@gmail.com>
2020-07-27 20:59:18 +1200

    add css for about section

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 export default function About() {
     return (
         <>
-        <div className="container">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod expedita eos accusantium vero perspiciatis repudiandae libero laudantium quidem, sit assumenda! Eligendi esse omnis possimus mollitia ipsum vel nobis nesciunt deleniti.</p>
-        </div>
+            <div className="container-about">
+                <div className="about-header">
+                    <h1> About </h1>
+                </div>
+                <div className="about-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut temporibus asperiores modi praesentium reprehenderit placeat, voluptatem quisquam, ex animi consequatur quod fugiat aperiam ea corrupti laborum ab fuga eveniet qui!</p>
+                </div>
+            </div>
         </>
     )
 }
