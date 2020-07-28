57b6b6c50638a4d362d86d9f9479b6643caa5465
Kevin Guan <kevinguan97@gmail.com>
2020-07-29 10:10:10 +1200

    style list items

--- client/components/RecipeList.jsx
+++ client/components/RecipeList.jsx
@@ -4,19 +4,26 @@ import { Link } from 'react-router-dom'
 
 function RecipeList(props) {
   return (
-    <div className="recipe-list">
+    <>
       <h1>Recipes</h1>
-      <ul>
-        {props.recipes.map(recipe => 
-        <li key={recipe.id}>
+      <div className="recipe-list">
+        <ul>
+          {props.recipes.map(recipe =>
+            <li key=
+              {recipe.id}>
               <Link to={{
-                pathname:`/recipe/${recipe.id}`,
-                state: { id: `${recipe.id}`}
-              }}>< img src={recipe.image} /></Link></li> 
-              
-        )}
-      </ul>
-    </div>
+                pathname: `/recipe/${recipe.id}`,
+                state: { id: `${recipe.id}` }
+              }}>
+                <div className="list-item box">
+                < img src={recipe.image} />{recipe.title}
+                </div>
+              </Link></li>
+
+          )}
+        </ul>
+      </div>
+    </>
   )
 }
 
